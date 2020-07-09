16cb8dac0a4906c48902003764ef1dbe2322182c
Pawel Szenk <pawel.szenk@softax.pl>
2020-07-09 13:04:43 +0200

    Dodanie testów jednostkowych

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -1,13 +1,89 @@
-import App
+@testable import App
+import Vapor
 import XCTest
 
+
 final class AppTests: XCTestCase {
-    func testNothing() throws {
-        // Add your tests here
+    
+    var app: Application!
+    
+    override func setUp() {
+        super.setUp()
+        app = try! Application.testable()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testListOfCoffehouses() throws {
+        let body: EmptyBody? = nil
+        let test = try app.sendRequest(to: "coffeehouses", method: .GET, body: body)
+        _ = try test.content.decode([CoffeeHouse].self).wait()
         XCTAssert(true)
     }
 
+    func testListOfCoffehousesLocations() throws {
+        let body: EmptyBody? = nil
+        let test = try app.sendRequest(to: "coffeehouses/1/locations", method: .GET, body: body)
+        _ = try test.content.decode([CoffeeHouseLocationReply].self).wait()
+        XCTAssert(true)
+    }
+    
+    func testCoffehousesMenu() throws {
+        let body: EmptyBody? = nil
+        let test = try app.sendRequest(to: "coffeehouses/1/menu", method: .GET, body: body)
+        _ = try test.content.decode(CoffeeHouseMenuReply.self).wait()
+        XCTAssert(true)
+    }
+    
+    func testProductDetails() throws {
+        let body: EmptyBody? = nil
+        let test = try app.sendRequest(to: "product/1", method: .GET, body: body)
+        _ = try test.content.decode(ProductReply.self).wait()
+        XCTAssert(true)
+    }
+    
+    func testSignup() throws {
+        let body = SignUpRequest(fullName: "Jan Nowak", phoneNumber: "+48123345567", city: "Warszawa", birthDay: "2010-01-01", gender: "MALE")
+        let test = try app.sendRequest(to: "signup", method: .POST, body: body)
+        _ = try test.content.decode(SignUpReply.self).wait()
+        XCTAssert(true)
+    }
+    
+    func testSignin() throws {
+        let empty: EmptyBody? = nil
+        let test = try app.sendRequest(to: "signin", method: .GET, body: empty)
+        let firstStepReply = try test.content.decode(SignInOneReply.self).wait()
+        
+        let user: SignInTwoRequest = SignInTwoRequest(appId: "fffae635-614e-27ca-bc20-f2e59f1b5bf3", crypto: "22222222")
+        var headers = HTTPHeaders()
+        headers.add(name: "X-Session-Id", value: firstStepReply.sessionId)
+        _ = try app.sendRequest(to: "signin", method: .POST, headers: headers, body: user)
+        XCTAssert(true)
+    }
+    
+    func testClient() throws {
+        let sessionId = try app.signIn(appId: "fffae635-614e-27ca-bc20-f2e59f1b5bf3", crypto: "22222222")
+        XCTAssertNotNil(sessionId)
+        
+        let empty: EmptyBody? = nil
+        var headers = HTTPHeaders()
+        headers.add(name: "X-Session-Id", value: sessionId!)
+        _ = try app.sendRequest(to: "client", method: .GET, headers: headers, body: empty)
+    }
+    
+    
+
     static let allTests = [
-        ("testNothing", testNothing)
+        ("testListOfCoffehouses", testListOfCoffehouses),
+        ("testListOfCoffehousesLocations", testListOfCoffehousesLocations),
+        ("testCoffehousesMenu", testCoffehousesMenu),
+        ("testProductDetails", testProductDetails),
+        ("testSignup", testSignup),
+        ("testSignin", testSignin)
+        
     ]
+    
+    
 }
---
+++ Tests/AppTests/Application+Testable.swift
@@ -0,0 +1,56 @@
+//
+//  Application+Testable.swift
+//  App
+//
+//  Created by Pawel Szenk on 09/07/2020.
+//
+
+import Foundation
+import Vapor
+
+extension Application {
+    
+    public static func testable() throws -> Application {
+         var config = Config.default()
+         var services = Services.default()
+         var env = Environment.testing
+         try App.configure(&config, &env, &services)
+         let app = try Application(config: config, environment: env, services: services)
+         try App.boot(app)
+
+         return app
+    }
+    
+    public func sendRequest<Body>(to path: String, method: HTTPMethod, headers: HTTPHeaders = .init(), body: Body?) throws -> Response where Body: Content {
+        let httpRequest = HTTPRequest(method: method, url: URL(string: path)!, headers: headers)
+        let wrappedRequest = Request(http: httpRequest, using: self)
+        if let body = body {
+            try wrappedRequest.content.encode(body)
+        }
+        print("==============================")
+        print("Send Request:")
+        print(wrappedRequest)
+        print("------------------------------")
+        let responder = try make(Responder.self)
+        let result = try responder.respond(to: wrappedRequest).wait()
+        print("------------------------------")
+        print("Reply:")
+        print(result)
+        return result
+    }
+    
+    public func signIn(appId: String, crypto: String) throws -> String? {
+        let empty: EmptyBody? = nil
+        let test = try self.sendRequest(to: "signin", method: .GET, body: empty)
+        let firstStepReply = try test.content.decode(SignInOneReply.self).wait()
+        
+        let user: SignInTwoRequest = SignInTwoRequest(appId: appId, crypto: crypto)
+        var headers = HTTPHeaders()
+        headers.add(name: "X-Session-Id", value: firstStepReply.sessionId)
+        _ = try self.sendRequest(to: "signin", method: .POST, headers: headers, body: user)
+        return firstStepReply.sessionId
+    }
+    
+}
+
+public struct EmptyBody: Content {}
