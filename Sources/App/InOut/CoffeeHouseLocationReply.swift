c0d396452cfbf8529794ce0bacc5a46c738ea981
Pawel Szenk <pawel.szenk@softax.pl>
2020-07-20 22:25:38 +0200

    Poprawki do API

--- Sources/App/InOut/CoffeeHouseLocationReply.swift
+++ Sources/App/InOut/CoffeeHouseLocationReply.swift
@@ -14,12 +14,23 @@ struct GSMLocation: Codable {
     var longitude: Double
 }
 
+struct OpeningHour: Codable {
+    var monday: String
+    var tuesday: String
+    var wednesday: String
+    var thursday: String
+    var friday: String
+    var saturday: String
+    var sunday: String
+}
+
 final class CoffeeHouseLocationReply: Content {
     var id: Int
     var name: String
     var address: PLAddress
     var phone: String
     var location: GSMLocation
+    var openingHours: OpeningHour
     
     init(location: Location) {
         self.id = location.id ?? 0
@@ -27,6 +38,7 @@ final class CoffeeHouseLocationReply: Content {
         self.address = PLAddress(city: location.city, postalCode: location.postalCode, post: location.post, street: location.street, house: location.house, flat: location.flat)
         self.phone = location.phone ?? ""
         self.location = GSMLocation(latitude: Double(location.latitude) ?? 0.0, longitude: Double(location.longitude) ?? 0.0)
+        self.openingHours = OpeningHour(monday: location.monday, tuesday: location.tuesday, wednesday: location.wednesday, thursday: location.thursday, friday: location.friday, saturday: location.saturday, sunday: location.sunday)
     }
 }
 
--- Sources/App/Models/Location.swift
+++ Sources/App/Models/Location.swift
@@ -15,10 +15,17 @@ final class Location: MySQLModel {
     var phone: String?
     var latitude: String
     var longitude: String
+    var monday: String
+    var tuesday: String
+    var wednesday: String
+    var thursday: String
+    var friday: String
+    var saturday: String
+    var sunday: String
     
     var coffeehouseID: Int
 
-    init(id: Int? = nil, name: String, city: String, postalCode: String, street: String?, house: String, flat: String?, post: String?, phone: String?, latitude: String, longitude: String, coffeehouseID: Int) {
+    init(id: Int? = nil, name: String, city: String, postalCode: String, street: String?, house: String, flat: String?, post: String?, phone: String?, latitude: String, longitude: String, monday: String, tuesday: String, wednesday: String, thursday: String, friday: String, saturday: String, sunday: String, coffeehouseID: Int) {
         self.id = id
         self.name = name
         self.city = city
@@ -29,6 +36,13 @@ final class Location: MySQLModel {
         self.phone = phone
         self.latitude = latitude
         self.longitude = longitude
+        self.monday = monday
+        self.tuesday = tuesday
+        self.wednesday = wednesday
+        self.thursday = thursday
+        self.friday = friday
+        self.saturday = saturday
+        self.sunday = sunday
         self.coffeehouseID = coffeehouseID
     }
 }
